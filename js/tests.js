24016ce465affaa065d1d1811b08ab5d7805d054
Nancy-Trigoso <nancytrigoso1998@gmail.com>
2024-06-18 15:53:17 -0500

    Resolviendo problema 02.

--- js/tests.js
+++ js/tests.js
@@ -10,6 +10,8 @@ function testSum() { //eslint-disable-line
   }
 }
 
+testSum();
+
 function testMultiply() { //eslint-disable-line
   if (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
     console.log('%c TEST FOR multiply() PASSES', 'color: green');
@@ -17,6 +19,8 @@ function testMultiply() { //eslint-disable-line
     console.log('%c TEST FOR multiply() FAILS', 'color: red');
   }
 }
+
+testMultiply();
 
 function testSumAndMultiply() { //eslint-disable-line
   if (sumAndMultiply(4, 7, 5)[0] === 16 && sumAndMultiply(4, 7, 5)[1] === 140 && sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
